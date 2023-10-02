1d2e03df02b0399c7e577c69982b51a1962f502a
Nathan Wooddell <Nathanwooddell@gmail.com>
2023-10-02 03:07:16 -0400

    Update mycookies.py

--- mycookies.py
+++ mycookies.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # Nathan Wooddell     ----     TC56612
 
+# This is heavily based off of the provided cookiex.py file.
+
 # Import Statements
 from os import environ                                          # Used in the retrevial of cookies
 import cgi                                                      # Imports CGI
@@ -15,15 +17,47 @@ ck2_value = ''
 ck3_name = 'dept'
 ck3_value = ''
 
+ck4_name = 'course'
+ck4_value = ''
 
+# create a cookie object and then get the environment for an HTTP cookie
+user_cookie = cookies.SimpleCookie()
+cookie_string = environ.get('HTTP_COOKIE')
 
-# Cookies must be sent before declaring the content type
-#print("Set-Cookie: Expires")
-
+if cookie_string is not None and cookie_string != "":
+    cookie_arr = cookie_string.split(';')
+    for cookie_item in cookie_arr:
+        cookie_item = cookie_item.strip()
+        (cookie_key, cookie_value) = cookie_item.split("=")
+        if cookie_key == ck1_name:
+            ck1_value = cookie_value
+        if cookie_key == ck2_name:
+            ck2_value = cookie_value
+        if cookie_key == ck3_name:
+            ck3_value == cookie_value
+        if cookie_key == ck4_name:
+            ck4_value == cookie_value
 
 # Headers for my HTML page
 print("Content-type: text/html")                                # Defines content type
 print("Host: mywebserver")                                      # Defines server host
+
+# Cookies must be declared before the body of the html, but after the headers
+Cookie_form = cgi.FieldStorage()                                # Gathers the form from the doc below
+if ck1_name in Cookie_form:                                     # checking for the umbcid cookie
+    umbcid = Cookie_form[ck1_name].value
+    print(f"Set-Cookie:{ck1_name} = {umbcid}; Max-Age = {30}")  # Will expire after 30 seconds
+if ck2_name in Cookie_form:
+    name = Cookie_form[ck2_name].value
+    print(f'Set-Cookie:{ck2_name} = {name}; Max-Age = {300}')   # Will expire after 300 seconds
+if ck3_name in Cookie_form:
+    name = Cookie_form[ck3_name].value
+    print(f'Set-Cookie:{ck3_name} = {name}; Domain = tc56612.umbc.local')
+if ck4_name in Cookie_form:
+    name = Cookie_form[ck4_name].value
+    print(f'Set-Cookie:{ck4_name} = {name}; Path = /tc56612')
+
+
 
 # Seperate Headers from content
 print()
@@ -70,10 +104,24 @@ print("http://TC56612.umbc.local:32769/welcome.html <br>")
 print("Otherwise the cookie should not be sent. This will require me to make an entry for domains on my host.<br>")
 
 # Form to set cookie variables
-print('<br><form action = "cgi-bin/mycookies.py" method = "POST" target = "_blank">')
+print('<br><form action = "/cgi-bin/mycookies.py" method = "POST" target = "_blank">')
 print(ck3_name)
 print(f'<br><input type="text" name="{ck3_name}" value="{ck3_value}" /><br>')
-print('<br><input type="submit" value="Create Cookie" />')
+print('<br><input type = "submit" value = "Create Cookie" />')
+print('</form>')
+
+
+
+# cookie course - will only be sent when accessing a webpage with a certain path
+print('<br><br> <h2>course Cookie Creation</h2>')
+print('A cookie will be created with a path value /TC56612 <br>')
+print('This path value will prevent the cookie from being sent when the a url outside of this one is visited.<br>')
+
+# Form to set cookie variables
+print('<br><form action = "/cgi-bin/mycookies.py" method = "POST" target = "_blank">')
+print(ck4_name)
+print(f'<br><input type="text" name="{ck4_name}" value="{ck4_value}" /><br>')
+print('<br><input type = "submit" value = "Create Cookie" />')
 print('</form>')
 
 
